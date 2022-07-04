d0a31a0825b6d4d182a0474373eba16537d1f815
Tasha Syarifah <66051012+tashasyarifah@users.noreply.github.com>
2022-07-04 15:51:42 +0700

    Solve API Bug

--- src/components/Advise.jsx
+++ src/components/Advise.jsx
@@ -1,18 +1,21 @@
-
 import { useEffect, useState } from "react"
 import styled from "styled-components"
 
-//Styles for the overall app
-//This is for the div
+
+// Styles for the overall app
+
+// This is for the div
 const Container = styled.div `
     background-color: hsl(217, 19%, 24%);
-    display: grid;
-    place-items: center;
     border-radius: 6%;
     outline: none;
-    padding: 80px;
+    display: grid;
+    place-content: center;
+    gap: 1ch;
+    // border: 2px solid white;
+    padding: 1.75rem 1rem
 `
-//This is for a small text above the quote
+// This is for a small text above the quote
 const SmallText = styled.h1`
     font-size: 0.8em;
     color: hsl(150, 100%, 66%);
@@ -20,12 +23,13 @@ const SmallText = styled.h1`
     padding-bottom: 30px;
     letter-spacing: 0.3em;
 `
-//It is a style for the random advise quote
+// It is a style for the random advise quote
 const Quote = styled.p`
     color: hsl(193, 38%, 86%);
     font-size: 1em;
 `
-//For the button
+
+// For the button
 const Button = styled.button`
     border-radius: 50%;
     height: 60px;
@@ -39,35 +43,30 @@ const Button = styled.button`
         box-shadow: 0 0 10px 9px hsl(150, 50%, 60%);
     }
 `
-//It is for the icon inside the button
+// It is for the icon inside the button
 const Icon = styled.svg`
     width: 24px;
     height: 24px;
 
 `
 export default function Advise() {
-    const [getId, setGetId] = useState("")
-    const [getAdvice, setGetAdvice] = useState("")
+    const [getApi, setGetApi] = useState({ id: "", advice:"" })
+
+    const getAdvices = async () => {
+        const res = await fetch("https://api.adviceslip.com/advice")
+        const data = await res.json()
+        setGetApi(data.slip)
+    }
 
     useEffect(() => {
-        async function getAdvices() {
-            const res = await fetch("https://api.adviceslip.com/advice")
-            const data = await res.json()
-            setGetId(data.slip.id)
-            setGetAdvice(data.slip.advice)
-        } 
         getAdvices()
     }, [])
 
-        function getRandomAdvice() {
-            console.log("clicked")
-        }
-
     return (
         <Container>
-            <SmallText>Advice #{getId}</SmallText>
-            <Quote>{getAdvice}</Quote>
-            <Button onClick={getRandomAdvice}>  
+            <SmallText>Advice #{getApi.id}</SmallText>
+            <Quote>{getApi.advice}</Quote>
+            <Button onClick={getAdvices}>  
                 <Icon>
                     <path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/>
                 </Icon>
